37c3c878101e706c6b9592d7e5cccce1ca649a5c
Aryan214G <aryan214gm@gmail.com>
2026-05-25 15:21:27 +0530

    Add form and output section elements to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// ________ Form section _________________
+
+const nameField = document.getElementById("name-field");
+const jobField = document.getElementById("job-role-field");
+const targetCompanyField = document.getElementById("target-company-field");
+const skillsArea = document.getElementById("skills");
+const generateBtn = document.getElementById("generate-btn");
+
+// ________ Output section _________________
+
+const coverLetter = document.getElementById("cover-letter-output");
+const copyBtn = document.getElementById("copy-btn");
+
