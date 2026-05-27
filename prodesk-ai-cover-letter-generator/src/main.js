c538818d9c73d4d32334a54c2e0b1cab635dd17e
Aryan214G <aryan214gm@gmail.com>
2026-05-27 23:24:10 +0530

    Refactor HTML structure, enhance styling, and implement cover letter generation functionality

--- prodesk-ai-cover-letter-generator/src/main.js
+++ prodesk-ai-cover-letter-generator/src/main.js
@@ -1,60 +1,69 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
 
-<div class="ticks"></div>
+// ________ Form section _________________
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+const nameField = document.getElementById("name-field");
+const jobField = document.getElementById("job-role-field");
+const targetCompanyField = document.getElementById("target-company-field");
+const skillsArea = document.getElementById("skills");
+const generateBtn = document.getElementById("generate-btn");
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+// ________ Output section _________________
 
-setupCounter(document.querySelector('#counter'))
+const coverLetter = document.getElementById("cover-letter-output");
+const copyBtn = document.getElementById("copy-btn");
+
+
+generateBtn.addEventListener("click", () => {
+    generateCoverLetter();
+})
+
+function generateCoverLetter() {
+
+    const company = targetCompanyField.value;
+    const name = nameField.value;
+    const role = jobField.value;
+    const skills = skillsArea.value;
+    
+    const letter = `
+        Dear Hiring Manager at ${company},
+
+        My name is ${name} and I am excited to apply for the ${role} role.
+
+        My key skills include ${skills}.
+
+        Thank you for your consideration.
+
+        Sincerely,
+        ${name}
+        `;
+
+        coverLetter.value = letter;
+        autoReizeTextArea();
+}
+
+
+copyBtn.addEventListener("click", () => {
+
+    navigator.clipboard.writeText(
+        coverLetter.value
+    );
+
+    copyBtn.textContent = "Copied!";
+
+    setTimeout(() => {
+
+        copyBtn.textContent =
+            "Copy to Clipboard";
+
+    }, 2000);
+});
+
+
+function autoReizeTextArea() {
+    
+    coverLetter.style.height = "auto";
+
+    coverLetter.style.height = coverLetter.scrollHeight + "px";
+}
